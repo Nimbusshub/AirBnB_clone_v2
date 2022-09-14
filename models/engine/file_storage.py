22b21b2f22c4dc934fb68b993922e4e337cce9fc
Nimah <busayonimota1603@gmail.com>
2022-09-14 23:07:57 +0100

    Update file torage too delete objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,15 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """Delete object from __objects"""
+        if obj is None:
+            return
+
+        key_obj = obj.to_dict()['__class__'] + '.' + obj.id
+        if key_obj in type(self).__objects.keys():
+            del type(self).__objects[key_obj]
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -36,15 +45,15 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
