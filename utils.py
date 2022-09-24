0a52c067d18c27d6a8a8fde3364c4d757d67bd8a
Nimah <busayonimota1603@gmail.com>
2022-09-24 18:51:43 +0100

    [Update]: stringify numbers preceeded with zeros

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 def is_floatstring(value):
+    if value[0] == '0':
+        return False
     try:
         float(value)
         return True
-    
+
     except ValueError:
         try:
             str(value)
             return False
         except Exception:
-            return 1
+            return
