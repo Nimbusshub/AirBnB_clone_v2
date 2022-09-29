5c846589851d47f7614f12d11498514840925453
Nimah <busayonimota1603@gmail.com>
2022-09-29 09:54:36 +0100

    [Feats]: Create all necessary directories and files

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+apt-get install -y nginx > /dev/null
+
+# Create all necessary directories and file
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "Hello World again!" > /data/web_static/releases/test/index.html
+
+# Check if directory current exist
+if [ -d "/data/web_static/current" ]
+then
+        sudo rm -rf /data/web_static/current
+fi
+# Create a symbolic link to test
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Change ownership to user ubuntu
+chown -hR "$USER":"$USER" /data
+
+# Configure nginx to serve content pointed to by symbolic link to hbnb_static
+sed -i '19i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart server
+service nginx restart
