a40e4fe1582cb580ef0587fc015f694bec5e9a2d
Nimah <busayonimota1603@gmail.com>
2022-09-29 10:15:52 +0100

    [Fix]: remove sudo and add a new line at the end of the file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get install -y nginx
+apt-get -y update > /dev/null
+apt-get install -y nginx > /dev/null
 
 # Create all necessary directories and file
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "Hello World again!" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "Hello World again!" > /data/web_static/releases/test/index.html
 
 # Check if directory current exist
 if [ -d "/data/web_static/current" ]
@@ -16,13 +16,13 @@ then
         sudo rm -rf /data/web_static/current
 fi
 # Create a symbolic link to test
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Change ownership to user ubuntu
-sudo chown -hR ubuntu:ubuntu /data
+chown -hR ubuntu:ubuntu /data
 
 # Configure nginx to serve content pointed to by symbolic link to hbnb_static
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart server
-sudo service nginx restart
+service nginx restart
