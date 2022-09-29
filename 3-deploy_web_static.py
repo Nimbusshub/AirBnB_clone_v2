2ecf1ba5797d7ab41edf68244aaff9eb1a24a64e
Nimah <busayonimota1603@gmail.com>
2022-09-29 18:42:47 +0100

    [Feats]: Pack and deploy all file

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Creates and distributes an archive to web servers,
+using created function deploy and pack"""
+from fabric.api import *
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['3.235.198.120', '3.239.50.204']
+
+
+def deploy():
+    """Pack and deploy all file """
+    file_path = do_pack()
+    if not file_path:
+        return False
+
+    run_cmd = do_deploy(file_path)
+    return run_cmd
